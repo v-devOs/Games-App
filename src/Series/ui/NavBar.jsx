c6b64614159ef5410ecc4b2416e54d92c8f09dc0
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-01-13 12:08:37 -0600

    Navbar responsive terminada

--- src/Series/ui/NavBar.jsx
+++ src/Series/ui/NavBar.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom"
 import '../styles/styles.css'
 import {FaBars} from 'react-icons/fa'
+import { useState } from "react"
 
 
 export const NavBar = () => {
+
+  const [isActive, setIsActive] = useState(false)
+
   return (
     <div className="header">
       <nav className="nav">
 
         <Link className="logo nav-link">Nombre Serie</Link>
 
-        <button className="toggle"> <FaBars /></button>
+        <button
+          onClick={ () => setIsActive( !isActive ) } 
+          className="nav-toggle"> <FaBars /></button>
 
-        <ul className="nav-menu">
+        <ul className={`nav-menu ${ isActive? 'nav-menu_visible' : ''  }`}>
           <li className="nav-menu-item"><Link to={''}  className="nav-menu-link nav-link">Buscar</Link></li>
           <li className="nav-menu-item"><Link to={''}  className="nav-menu-link nav-link">Jugar</Link></li>
           <li className="nav-menu-item"><Link to={'/'}  className="nav-menu-link nav-link">Inicio</Link></li>
